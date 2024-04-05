97f4a97b51f36ae594f3bb78ab7fa1ed3c2be637
sunabhasinha <mailsunabha@gmail.com>
2024-04-06 01:26:03 +0530

    Infinite Scroll

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+	import { ref, onMounted } from 'vue';
+	import Posts from './components/Posts.vue';
+
+	const posts = ref([]);
+
+	const fetchPosts = async () => {
+		const resJson = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const res = await resJson.json();
+		posts.value = [...posts.value, ...res];
+	};
+
+	onMounted(async () => {
+		await fetchPosts();
+		window.addEventListener('scroll', handleScroll);
+	});
+
+	const handleScroll = async () => {
+		if (
+			window.scrollY + window.innerHeight >=
+			document.body.scrollHeight - 50
+		) {
+			await fetchPosts();
+			console.log('Postsss', posts.value);
+		}
+	};
+</script>
+
+<template>
+	<Posts v-for="(post, i) in posts" :key="i" :post="post" />
+</template>
+
+<style scoped></style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,20 @@
+<script setup>
+	const props = defineProps(['post']);
+</script>
+
+<template>
+	<article>
+		<h1>{{ post.title }}</h1>
+		<p>{{ post.body }}</p>
+	</article>
+</template>
+
+<style scoped>
+	article {
+		border: 1px solid aqua;
+		border-radius: 16px;
+		margin: 20px 0;
+		width: 500px;
+		padding: 20px;
+	}
+</style>
